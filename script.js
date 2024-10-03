8d9c269d04632e2bf150b2c4384bf19dc634d808
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-10-03 22:23:07 +0530

    fix: add new error page

--- script.js
+++ script.js
@@ -6,38 +6,34 @@ const secEl = document.getElementById("sec");
 
 //create constant
 
-const newYears = "1 jan 2024";
+const newYears = "1 jan 2025";
 
 //creat funtion
 
 //js wla date different eka denne mili second ganakat
 //ekai 1000 bedanne
 
-function countdown()
-{
+function countdown() {
   const newYearDate = new Date(newYears);
-  const currentDate  = new Date();
+  const currentDate = new Date();
 
-  const totalseconds = Math.floor((newYearDate - currentDate)/1000);
+  const totalseconds = Math.floor((newYearDate - currentDate) / 1000);
 
-  const days = Math.floor(totalseconds /(3600 * 24));
+  const days = Math.floor(totalseconds / (3600 * 24));
 
-  const hours = Math.floor(totalseconds/3600) % 24;
+  const hours = Math.floor(totalseconds / 3600) % 24;
 
-  const minutes = Math.floor(totalseconds/60) % 60;
+  const minutes = Math.floor(totalseconds / 60) % 60;
 
   const second = Math.floor(totalseconds) % 60;
-
- 
 
   daysEl.innerHTML = days;
   minEl.innerHTML = minutes;
   hoursEl.innerHTML = hours;
   secEl.innerHTML = second;
-  console.log(days,hours,minutes,second);
+  console.log(days, hours, minutes, second);
 }
 
-
-//initail call funtion 
+//initail call funtion
 countdown();
-setInterval(countdown,1000);
+setInterval(countdown, 1000);
